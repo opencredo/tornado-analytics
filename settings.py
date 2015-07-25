0f5c31db537cb9491f2d53b1ba2f6552681920ec
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-07-25 23:32:00 +0100

    cleanup

--- settings.py
+++ settings.py
@@ -17,7 +17,4 @@ settings["login_url"] = "/login"
 settings["static_path"] = os.path.join(os.path.dirname(__file__), "static")
 settings["template_path"] = os.path.join(os.path.dirname(__file__), "templates")
 settings["xsrf_cookies"] = False
-settings["max_process_workers"] = 100
 settings["service_account_email"] = '***REMOVED***'
-# download client secrets file from your google developer console > auth > credentials
-# settings["key_file_location"] = 'client_secrets2.p12'
