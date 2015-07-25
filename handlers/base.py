45d2d7ac4c06c0fffefacd3534149e49f33135fd
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-07-25 14:10:49 +0100

    adding css and js files

--- handlers/base.py
+++ handlers/base.py
@@ -1,10 +1,70 @@
-# -*- coding: utf-8 -*-
+__author__ = 'karolisrusenas'
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial, wraps
 
+import tornado.ioloop
+import json
 import tornado.web
 
+import logging
+logger = logging.getLogger('quasar.' + __name__)
 
-class MainHandler(tornado.web.RequestHandler):
 
-    #@tornado.web.authenticated
-    def get(self):
-        self.render('index.html')
+class BaseHandler(tornado.web.RequestHandler):
+    """A class to collect common handler methods - all other handlers should
+    subclass this one.
+    """
+
+    def load_json(self):
+        """Load JSON from the request body and store them in
+        self.request.arguments, like Tornado does by default for POSTed form
+        parameters.
+        If JSON cannot be decoded, raises an HTTPError with status 400.
+        """
+        try:
+            self.request.arguments = json.loads(self.request.body)
+        except ValueError:
+            msg = "Could not decode JSON: %s" % self.request.body
+            logger.debug(msg)
+            raise tornado.web.HTTPError(400, msg)
+
+    def get_json_argument(self, name, default=None):
+        """Find and return the argument with key 'name' from JSON request data.
+        Similar to Tornado's get_argument() method.
+        """
+        if default is None:
+            default = self._ARG_DEFAULT
+        if not self.request.arguments:
+            self.load_json()
+        if name not in self.request.arguments:
+            if default is self._ARG_DEFAULT:
+                msg = "Missing argument '%s'" % name
+                logger.debug(msg)
+                raise tornado.web.HTTPError(400, msg)
+            logger.debug("Returning default argument %s, as we couldn't find "
+                         "'%s' in %s" % (default, name, self.request.arguments))
+            return default
+        arg = self.request.arguments[name]
+        logger.debug("Found '%s': %s in JSON arguments" % (name, arg))
+        return arg
+
+
+def unblock(f):
+
+    @tornado.web.asynchronous
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        self = args[0]
+        EXECUTOR = ThreadPoolExecutor(self.settings['max_process_workers'])
+
+        def callback(future):
+            self.write(future.result())
+            self.finish()
+
+        EXECUTOR.submit(
+            partial(f, *args, **kwargs)
+        ).add_done_callback(
+            lambda future: tornado.ioloop.IOLoop.instance().add_callback(
+                partial(callback, future)))
+
+    return wrapper
---
+++ utilities/gaclient.py
@@ -0,0 +1,147 @@
+"""A simple example of how to access the Google Analytics API."""
+
+import argparse
+
+from googleapiclient.discovery import build
+from oauth2client.client import SignedJwtAssertionCredentials
+from pprint import pprint as pp
+import httplib2
+import os
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+
+
+class GAcess:
+
+    def __init__(self, scope=['https://www.googleapis.com/auth/analytics.readonly'], key_file_location=None,
+                 service_account_email=None):
+        if key_file_location is None:
+            key_file_location = os.path.dirname(os.path.realpath(__file__)) + '/' + 'client_secrets.p12'
+        # getting service object
+        self.service = self.get_service('analytics', 'v3', scope, key_file_location,
+                                        service_account_email)
+
+    def get_service(self, api_name, api_version, scope, key_file_location,
+                    service_account_email):
+        """Get a service that communicates to a Google API.
+
+        Args:
+          api_name: The name of the api to connect to.
+          api_version: The api version to connect to.
+          scope: A list auth scopes to authorize for the application.
+          key_file_location: The path to a valid service account p12 key file.
+          service_account_email: The service account email address.
+
+        Returns:
+          A service that is connected to the specified API.
+        """
+        f = open(key_file_location, 'rb')
+        key = f.read()
+        f.close()
+
+        credentials = SignedJwtAssertionCredentials(service_account_email, key,
+                                                    scope=scope)
+
+        http = credentials.authorize(httplib2.Http())
+
+        # Build the service object.
+        service = build(api_name, api_version, http=http)
+
+        return service
+
+    def get_first_profile_id(self):
+        # Use the Analytics service object to get the first profile id.
+
+        # Get a list of all Google Analytics accounts for this user
+        accounts = self.service.management().accounts().list().execute()
+
+        if accounts.get('items'):
+            # Get the first Google Analytics account.
+            account = accounts.get('items')[0].get('id')
+
+            # Get a list of all the properties for the first account.
+            properties = self.service.management().webproperties().list(
+                accountId=account).execute()
+
+            if properties.get('items'):
+                # Get the first property id.
+                property = properties.get('items')[0].get('id')
+
+                # Get a list of all views (profiles) for the first property.
+                profiles = self.service.management().profiles().list(
+                    accountId=account,
+                    webPropertyId=property).execute()
+
+                if profiles.get('items'):
+                    # return the first view (profile) id.
+                    return profiles.get('items')[0].get('id')
+
+        return None
+
+    def get_sessions_results(self, profile_id=None, days=30):
+        """
+         Use the Analytics Service Object to query the Core Reporting API
+        for the number of sessions within the past seven days.
+
+        :param profile_id: profile ID - leave blank to use the first one, otherwise provide string containing profile ID
+        :param days: optional, integer
+        :return:
+        """
+        # getting profile ID
+        if profile_id is None:
+            profile_id = self.get_first_profile_id()
+
+        return self.service.data().ga().get(
+            ids='ga:' + profile_id,
+            start_date='%sdaysAgo' % days,
+            end_date='today',
+            metrics='ga:sessions').execute()
+
+    def get_people_sources(self, profile_id=None, days=30, max_results=10):
+        """
+
+        :param profile_id: profile ID - leave blank to use the first one, otherwise provide string containing profile ID
+        :param days: optional, integer
+        :param max_results: optional, integer
+        :return:
+        """
+
+        # getting profile ID
+        if profile_id is None:
+            profile_id = self.get_first_profile_id()
+
+        results = self.service.data().ga().get(
+            ids='ga:' + profile_id,
+            start_date='%sdaysAgo' % days,
+            end_date='today',
+            metrics='ga:sessions,ga:pageviews,ga:sessionDuration,ga:exits',
+            dimensions='ga:source,ga:medium',
+            sort='-ga:sessions',
+            max_results=max_results).execute()
+
+        return results
+
+
+def main():
+    # Define the auth scopes to request.
+    scope = ['https://www.googleapis.com/auth/analytics.readonly']
+
+    # Use the developer console and replace the values with your
+    # service account email and relative location of your key file.
+    # don't forget to add your service account email address to your google analytics users!
+    service_account_email = '***REMOVED***'
+    # download client secrets file from your google developer console > auth > credentials
+    key_file_location = 'client_secrets2.p12'
+
+    # authenticate and construct service
+    #service = GAcess(scope, key_file_location, service_account_email)
+    service = GAcess(service_account_email=service_account_email)
+
+    # use service object to call commands (get_people_sources, etc..)
+    import pdb
+    pdb.set_trace()
+
+
+if __name__ == '__main__':
+    main()
