66b335d398d04427b4352d4debd46c28b193cab8
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-07-26 11:06:58 +0100

    added top pages handler

--- handlers/web_handlers.py
+++ handlers/web_handlers.py
@@ -103,3 +103,70 @@ class TopCountriesHandler(BaseHandler):
                                       data=data,
                                       table_title=table_title,
                                       headers=headers)
+
+
+class TopPagesHandler(BaseHandler):
+    def initialize(self):
+        service_account = self.settings['service_account_email']
+        self.service = GAcess(service_account_email=service_account)
+
+    @unblock
+    def get(self):
+        """
+        Returns posts that are most popular.
+
+        Example:
+        headers:
+         [{'columnType': 'DIMENSION',
+                    'dataType': 'STRING',
+                    'name': 'ga:pagePath'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'INTEGER',
+                    'name': 'ga:pageviews'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'INTEGER',
+                    'name': 'ga:uniquePageviews'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'TIME',
+                    'name': 'ga:timeOnPage'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'INTEGER',
+                    'name': 'ga:bounces'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'INTEGER',
+                    'name': 'ga:entrances'},
+                   {'columnType': 'METRIC',
+                    'dataType': 'INTEGER',
+                    'name': 'ga:exits'}],
+        rows:
+        [['/2015/07/08/a-deep-dive-into-angular-2-0/',
+           '4327',
+           '4043',
+           '126847.0',
+           '3741',
+           '4012',
+           '3993'],
+          ['/', '1892', '1689', '36065.0', '1095', '1590', '1191'],
+          ['/2014/02/24/experiences-with-spring-boot/',
+           '1166',
+           '1133',
+           '16328.0',
+           '1097',
+           '1129',
+           '1127'],
+
+        :return:
+        """
+        query_result = self.service.get_top_pages()
+
+        try:
+            data = query_result['rows']
+        except KeyError:
+            self.set_status(400, reason='Failed to fetch top countries data')
+        else:
+            table_title = 'Which posts are most popular?'
+            headers = ['Path', 'Page views', 'Unique views', 'Time on page', 'Bounces', 'Ent.', 'Exits']
+            return self.render_string('webhandler/data_table.html',
+                                      data=data,
+                                      table_title=table_title,
+                                      headers=headers)
--- urls.py
+++ urls.py
@@ -7,4 +7,5 @@ url_patterns = [
     (r"/", web_handlers.MainHandler),
     (r"/people-sources", web_handlers.PeopleSourcesHandler),
     (r"/top-countries", web_handlers.TopCountriesHandler),
+    (r"/top-pages", web_handlers.TopPagesHandler),
 ]
